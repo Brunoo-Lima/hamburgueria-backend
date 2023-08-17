38f86ca04af8ad641b5a5ea1b996f9e2e5e9faf4
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-08-16 21:30:23 -0300

    :feat: add error handling and cors

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import { Request, Response, Router } from 'express';
 const router = Router();
 
 router.get('/test', (req: Request, res: Response) => {
-  return res.json({ ok: true });
+  // return res.json({ ok: true });
+  throw new Error('Algo deu Errado!');
 });
 
 export { router };
