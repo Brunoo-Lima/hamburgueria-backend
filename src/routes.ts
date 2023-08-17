9db80fe967f8d9532bab1eec6062031a510b31a1
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-08-17 01:01:10 -0300

    :feat: add login user and token generation every time login

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from 'express';
 
 import { CreateUserController } from './controllers/users/CreateUserController';
+import { AuthUserController } from './controllers/users/AuthUserController';
 
 const router = Router();
 
+//--ROTAS USER--\\
 router.post('/user', new CreateUserController().handle);
+router.post('/session', new AuthUserController().handle);
 
 export { router };
