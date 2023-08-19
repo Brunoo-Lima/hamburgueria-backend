011fc6104fcc2ee390b0df4eac27dd29d58717f8
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-08-19 18:06:28 -0300

    :feat: finish order

--- src/routes.ts
+++ src/routes.ts
@@ -17,8 +17,10 @@ import { RemoveOrderController } from './controllers/order/RemoveOrderController
 import { AddItemsOrderController } from './controllers/order/AddItemsOrderController';
 import { RemoveItemOrderController } from './controllers/order/RemoveItemOrderController';
 import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
 
 import uploadConfig from './config/multer';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 const router = Router();
 
@@ -50,15 +52,21 @@ router.get('/product', isAuthenticated, new ListByCategoryController().handle);
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 router.post(
-  '/order/item',
+  '/order/add',
   isAuthenticated,
   new AddItemsOrderController().handle,
 );
 router.delete(
-  '/order/item',
+  '/order/remove',
   isAuthenticated,
   new RemoveItemOrderController().handle,
 );
 router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+router.get('/orders', isAuthenticated, new ListOrderController().handle);
+router.put(
+  '/order/finish',
+  isAuthenticated,
+  new FinishOrderController().handle,
+);
 
 export { router };
