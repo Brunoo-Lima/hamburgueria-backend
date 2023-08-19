002631cc4ca11940db5586f4f64f3dab1c267d50
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2023-08-19 14:24:46 -0300

    :feat: add router static for image preview

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import path from 'path';
 
 import { router } from './routes';
 
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+// rota para visualizar imagem no navegador - http://localhost:3000/files/49b1e4af1303e311c445baa35a4fcb42-sorvete.jpeg
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
